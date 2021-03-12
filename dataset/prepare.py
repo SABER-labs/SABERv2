5c8ef9dcfcdcaec1f8254d8b5154602b7372219c
Arjun Variar <990443+viig99@users.noreply.github.com>
2021-03-12 22:30:33 +0530

    Files filtered.

--- dataset/prepare.py
+++ dataset/prepare.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     supervised_dataset.to_csv(supervised_path, sep="\t", quoting=csv.QUOTE_NONE, header=True, index=False)
     print(f"Unsupervised set created at {unsupervised_path} with {len(unsupervised_dataset)} files.")
     print(f"Supervised set created at {supervised_path} with {len(supervised_dataset)} files.")
+    print(f"Num files filtered = {len(dataset) - len(unsupervised_dataset) - len(supervised_dataset)}")
