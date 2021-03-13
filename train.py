85d3bf7c744eabcdf53754eaf9a9684e17ea9283
Arjun Variar <990443+viig99@users.noreply.github.com>
2021-03-14 00:19:13 +0530

    Adding find_unused_parameters=False as per the recommendation.

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from utils.config import config
 from bolts.unsupervised_data import UnsupervisedCommonVoiceDataModule
 from bolts.simclr import SpeechSimClr
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.plugins import DDPShardedPlugin
 
 simclr_datamodule = UnsupervisedCommonVoiceDataModule()
 simclr_datamodule.prepare_data()
@@ -12,32 +13,34 @@ simclr_datamodule.setup(stage='fit')
 
 simclr = SpeechSimClr(num_train_samples=simclr_datamodule.num_train_samples())
 logger = TensorBoardLogger(os.path.join(
-	config.trainer.default_root_dir, config.trainer.tensorboard_logdir), name='simclr')
+    config.trainer.default_root_dir, config.trainer.tensorboard_logdir), name='simclr')
 
 model_checkpoint = ModelCheckpoint(
-	dirpath=os.path.join(config.trainer.default_root_dir, config.trainer.savewieghts_dir), 
-	save_last=True, 
-	save_top_k=1, 
-	monitor='train_loss'
+    dirpath=os.path.join(config.trainer.default_root_dir,
+                         config.trainer.savewieghts_dir),
+    save_last=True,
+    save_top_k=1,
+    monitor='train_loss'
 )
 lr_monitor = LearningRateMonitor(logging_interval='step')
 callbacks = [model_checkpoint, lr_monitor]
 
 trainer = pl.Trainer(
-	default_root_dir=config.trainer.default_root_dir,
-	gpus=config.trainer.num_gpus,
-	max_epochs=config.trainer.max_epochs,
-	accelerator='ddp' if config.trainer.num_gpus > 1 else None,
-	plugins='ddp_sharded' if config.trainer.num_gpus > 1 else None,
-	num_nodes=config.trainer.num_nodes,
-	log_every_n_steps=config.trainer.log_every_n_steps,
-	gradient_clip_val=config.trainer.gradient_clip_val,
-	precision=config.trainer.precision,
-	callbacks=callbacks,
-	fast_dev_run=config.trainer.fast_dev_run,
-	logger=logger,
-	terminate_on_nan=False,
-	# resume_from_checkpoint=os.path.join(config.trainer.default_root_dir, config.trainer.savewieghts_dir)
+    default_root_dir=config.trainer.default_root_dir,
+    gpus=config.trainer.num_gpus,
+    max_epochs=config.trainer.max_epochs,
+    accelerator='ddp' if config.trainer.num_gpus > 1 else None,
+    plugins=DDPShardedPlugin(
+        find_unused_parameters=False) if config.trainer.num_gpus > 1 else None,
+    num_nodes=config.trainer.num_nodes,
+    log_every_n_steps=config.trainer.log_every_n_steps,
+    gradient_clip_val=config.trainer.gradient_clip_val,
+    precision=config.trainer.precision,
+    callbacks=callbacks,
+    fast_dev_run=config.trainer.fast_dev_run,
+    logger=logger,
+    terminate_on_nan=False,
+    # resume_from_checkpoint=os.path.join(config.trainer.default_root_dir, config.trainer.savewieghts_dir)
 )
 
 trainer.fit(simclr, datamodule=simclr_datamodule)
