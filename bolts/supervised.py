45cde08e33e3be716021f8ca61608682160b99ca
Arjun Variar <Arjun.Variar@airtel.com>
2021-05-13 17:40:25 +0530

    Dont add to progress bar.

--- bolts/supervised.py
+++ bolts/supervised.py
@@ -101,9 +101,9 @@ class SupervisedTask(pl.LightningModule):
         avg_val_loss = torch.stack([x['val_loss'] for x in output]).mean()
         avg_error = mean([x['wer'] for x in output])
         avg_cer = mean([x['cer'] for x in output])
-        self.log('val_loss', avg_val_loss, prog_bar=True)
-        self.log('val_wer', avg_error, prog_bar=True)
-        self.log('val_cer', avg_cer, prog_bar=True)
+        self.log('val_loss', avg_val_loss)
+        self.log('val_wer', avg_error)
+        self.log('val_cer', avg_cer)
 
     def get_progress_bar_dict(self):
         items = super().get_progress_bar_dict()
